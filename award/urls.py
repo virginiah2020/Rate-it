a749473755cf239b2125e50dcd8f488a4bca4819
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-19 23:05:36 +0300

    imported views on url.py

--- award/urls.py
+++ award/urls.py
@@ -1,22 +1,4 @@
 from django.conf.urls import url, include
-from django.contrib import admin
-from django.contrib.auth import views
-from registration.backends.simple.views import RegistrationView
-from awwards.forms import RegisterForm
-
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('awwards.urls')),
-    url(r'^accounts/register/$',
-        RegistrationView.as_view(
-            form_class=RegisterForm
-        ),
-        name='registration_register',
-    ),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'}), 
-    url(r'^tinymce/', include('tinymce.urls')),
-
-
-]
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
