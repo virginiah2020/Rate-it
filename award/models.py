d67b7ee0109e4140aed52ed8e55e2d4beb2c1bec
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-19 22:38:37 +0300

    added class profile in models

--- award/models.py
+++ award/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = models.ImageField(upload_to = 'profile_pics/', blank=True, default='profile_pics/default.jpg')
+
+    def save_profile(self):
+        self.save()
+        
+        img = Image.open(self.photo.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
+            
+
+    def delete_profile(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.bio
+    
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
--- awards/urls.py
+++ awards/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^',include('award.urls')),
+    # path(r'^',include('award.urls')),
 ]
