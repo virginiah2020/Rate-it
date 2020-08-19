f45ac42b393ec1dfa2e421fe5e40093ce97521ca
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-19 22:58:24 +0300

    pip install django-tinymce

--- awards/settings.py
+++ awards/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'award',
-    'bootstrap4'
+    'bootstrap4',
     'django_countries',
     'star_ratings',
 ]
