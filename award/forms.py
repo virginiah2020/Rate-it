2c70bd6cd2bfaae7e36a97c81eef8ca884538a7b
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-19 23:10:15 +0300

    imported registration form

---
+++ award/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from registration.forms import RegistrationForm
+from crispy_forms.helper import FormHelper
