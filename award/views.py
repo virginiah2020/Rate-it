82db625740b6cc50f95f99938226fe404251e2bb
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-19 22:21:33 +0300

    added def index

--- award/views.py
+++ award/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 # Create your views here.
+def index(request):
+    date = dt.date.today()
+    projects = Projects.get_projects()
+    
+
+    return render(request, 'index.html', {"date": date, "projects":projects})
