9dbc08568ba463fd95afd888e0a6e3c8a73b69e8
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-19 23:00:56 +0300

    added media root

--- awards/settings.py
+++ awards/settings.py
@@ -129,3 +129,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL ='/'
+STAR_RATINGS_RANGE = 10 
