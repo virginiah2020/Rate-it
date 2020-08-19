19ac207fc86f0ee66fb225edbc9de68ec27c3c36
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-19 23:03:04 +0300

    added urlpattern in project

---
+++ award/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth import views
+from registration.backends.simple.views import RegistrationView
+from awwards.forms import RegisterForm
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('awwards.urls')),
+    url(r'^accounts/register/$',
+        RegistrationView.as_view(
+            form_class=RegisterForm
+        ),
+        name='registration_register',
+    ),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    url(r'^tinymce/', include('tinymce.urls')),
+
+
+]
